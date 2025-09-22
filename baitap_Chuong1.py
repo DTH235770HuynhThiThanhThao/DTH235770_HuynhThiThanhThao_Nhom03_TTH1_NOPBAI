2f67e8462bebcb8c837ce91c17ce285ec2a51fcb
DTH235770HuynhThiThanhThao <thaohuynh1092005@gmail.com>
2025-09-22 10:19:27 +0700

    Add files via upload

---
+++ baitap_Chuong1.py
@@ -0,0 +1,28 @@
+# câu 3, cac VD
+print("hello world !")
+
+print("Albert Esintein đã từng nói: Một người không bao giờ mắc sai lầm không bao giờ thử bất kỳ điều gì mới.")
+
+if 5>2:
+    print("Five is greater than you")
+
+total =1+\
+2+\
+3
+print(total); print("Hello Python!")
+
+if True:
+    print("True")
+else:
+    print("False")
+
+import math
+import os
+
+
+#cau 4
+print("Chao cac ban !")
+#cau 5
+name=input("Moi ban nhap ho va ten: ")
+print("Xin chao: ",name)
+print("Co len nha")
